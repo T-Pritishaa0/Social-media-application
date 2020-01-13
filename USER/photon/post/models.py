5fd022a80e4cbec5726bf2f2f26221ce7c8b25b9
PritishaTuladhar <58884258+PritishaTuladhar@users.noreply.github.com>
2020-01-13 17:11:09 +0545

    new

--- USER/photon/post/models.py
+++ USER/photon/post/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Post(models.Model):
 	Title = models.CharField(max_length=100)
 	Caption = models.CharField(max_length=100)
-	File = models.FileField(upload_to="profile/photo/")
+	File = models.FileField(upload_to="profile/photos/")
 
 	def __str__(self):
 		return self.Title
