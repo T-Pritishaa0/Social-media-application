4e0f3d6c279443722c5689d929a7f4418c4ee32a
PritishaTuladhar <58884258+PritishaTuladhar@users.noreply.github.com>
2020-01-30 10:41:21 +0545

    changes

--- USER/photon/post/models.py
+++ USER/photon/post/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
 	Title = models.CharField(max_length=100)
 	Caption = models.CharField(max_length=100)
 	File = models.FileField(upload_to="profile/photos/")
-
+    
 	def __str__(self):
 		return self.Title
 
