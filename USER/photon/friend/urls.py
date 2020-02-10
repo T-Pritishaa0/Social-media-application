837aff3b9444acad3e3e3cd0eeba436aa7ac0e4b
sabu tha shrestha <np03a180227@heraldcollege.edu.np>
2020-02-10 08:36:44 +0545

    Update urls.py

--- USER/photon/friend/urls.py
+++ USER/photon/friend/urls.py
@@ -3,10 +3,12 @@ from django.conf import settings
 from django.conf.urls import url
 from django.urls import path
 from . import views
+from friend.views import FriendView
 
 app_name = "friend"
 
 urlpatterns = [
-	url(r'^$', FriendView.as_view(), name ='friend')
-	url(r'^connect/(?P<operation>.+)/(?P<pk>\d+)/$', views.change_friends, name='change_friends')
-	]
+    url(r'^$', FriendView.as_view(), name ='friend'),
+    url(r'^connect/(?P<operation>.+)/(?P<pk>\d+)/$', views.change_friends, name='change_friends'),
+    
+    ]
