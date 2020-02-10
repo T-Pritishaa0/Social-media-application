61d6925b406702be49b074a97d68af8b09b72356
sabu tha shrestha <np03a180227@heraldcollege.edu.np>
2020-02-10 08:34:25 +0545

    Update models.py

--- USER/photon/friend/models.py
+++ USER/photon/friend/models.py
@@ -1,16 +1,10 @@
+from django.conf import settings
 from django.db import models
 from main.models import User
 
 # Create your models here.
 
 class Friend(models.Model):
-	#user = models.ForeignKey(User)
-	users = models.ManyToManyField(User, related_name="friends")
-	current_user = models.ForeignKey(User, related_name='owner', null=True)
-
-   
-
-
-
-
-
+    users = models.ManyToManyField(User, related_name="friends")
+    current_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner', null=True)
+    #status = models.CharField(max_length=20, default='requested')
