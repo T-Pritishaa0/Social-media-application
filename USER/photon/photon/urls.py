402ed03dc83915f9886753f2fac3f8423af7ed49
sabu tha shrestha <np03a180227@heraldcollege.edu.np>
2020-02-10 08:38:51 +0545

    Update urls.py

--- USER/photon/photon/urls.py
+++ USER/photon/photon/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+#from myapp.views import FriendView
+
+
+app_name = "photon"
 
 
 urlpatterns = [
-	#path('main/', include('main.urls')),
+    path('', include('friend.urls')),
     path('admin/', admin.site.urls),
-]
+    ]
